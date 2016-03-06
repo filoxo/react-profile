dca63f7d38e66aef2a44ef9d942ed6f562e8705b
Carlos Filoteo <carlos_filoteo@byu.edu>
2016-03-05 17:20:13 -0700

    Add basic App component

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render(){
+		return (
+			<div id="layout" className="pure-g">
+				<div className="sidebar pure-u-1 pure-u-md-1-4">
+					<div className="header">
+						<h1 className="brand-title">Name Goeshere</h1>
+						<h2 className="brand-tagline">Web Developer</h2>
+						<nav className="nav">
+							<ul className="nav-list">
+								<li className="nav-item">
+									<a className="pure-button" href="//github.com">Github</a>
+								</li>
+								<li className="nav-item">
+									<a className="pure-button" href="//linkedin.com">LinkedIn</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<div className="content pure-u-1 pure-u-md-3-4">
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
