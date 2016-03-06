8bb7a2b4fbf8f5f2614a74f0b9fc6005bd2e5412
Carlos Filoteo <carlos_filoteo@byu.edu>
2016-03-05 17:23:53 -0700

    Rename

---
+++ Joblist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JobList extends Component {
+	render(){
+		return (
+			<div className="posts">
+				<h2 className="content-subhead">Experience</h2>
+			</div>
+		);
+	}
+}
+
+export default JobList;
