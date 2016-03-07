aaf17908d5f866039fae93c9cbe1bef0367fa027
Carlos Filoteo <carlos_filoteo@byu.edu>
2016-03-06 22:17:41 -0700

    Import Job component, render Job for every object in joblist array

--- Joblist.js
+++ Joblist.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Job from './Job';
 
 class JobList extends Component {
 	render(){
 		return (
 			<div className="posts">
 				<h2 className="content-subhead">Experience</h2>
+				{
+					this.props.jobs.map((jobData, ndx)=>{
+						return <Job key={ndx} data={jobData} />
+					})
+				}
 			</div>
 		);
 	}
